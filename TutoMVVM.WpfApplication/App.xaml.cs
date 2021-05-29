2321b40d54be66bac4ec0b1a61e78c060584e2bc
Treize <Jonathan.rougier@hotmail.fr>
2021-05-29 11:53:27 +0200

    Add WPF application

--- TutoMVVM.WpfApplication/App.xaml.cs
+++ TutoMVVM.WpfApplication/App.xaml.cs
@@ -6,12 +6,19 @@ using System.Linq;
 using System.Threading.Tasks;
 using System.Windows;
 
-namespace TutoMVVM
+namespace TutoMVVM.WpfApplication
 {
     /// <summary>
     /// Interaction logic for App.xaml
     /// </summary>
     public partial class App : Application
     {
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            MainWindow window = new MainWindow();
+            window.Show();
+
+            base.OnStartup(e);
+        }
     }
 }
--- TutoMVVM.WpfApplication/MainWindow.xaml.cs
+++ TutoMVVM.WpfApplication/MainWindow.xaml.cs
@@ -13,7 +13,7 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 
-namespace TutoMVVM
+namespace TutoMVVM.WpfApplication
 {
     /// <summary>
     /// Interaction logic for MainWindow.xaml
